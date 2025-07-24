e1684ea9fa7838a6a4cf67f54ff3f76a46809a31
Abraham Bishop <crypticcodetechnologies@gmail.com>
2025-07-24 14:43:59 +0100

    Added visit company and apply buttons

--- src/components/job-details-modal.jsx
+++ src/components/job-details-modal.jsx
@@ -10,9 +10,7 @@ import {
 import { Button } from "@/components/ui/button";
 import JobDescriptionPreview from "./job-desc-preview";
 
-const JobDetailsModal = ({
-    job
-}) => {
+const JobDetailsModal = ({ job }) => {
   return (
     <Dialog>
       <DialogTrigger>
@@ -21,15 +19,32 @@ const JobDetailsModal = ({
       <DialogContent className="min-w-[80%] max-h-[90vh] overflow-y-auto rounded-none">
         <DialogHeader>
           <div className="flex items-center gap-3 mb-5">
-              <img src={job.company_logo} alt={job.company} className="w-[90px] text-xs" />
-          <DialogTitle>{job.position}</DialogTitle>
+            <img
+              src={job.company_logo}
+              alt={job.company}
+              className="w-[90px] text-xs"
+            />
+            <DialogTitle>{job.position}</DialogTitle>
           </div>
+
+          {/* buttons */}
+          <div className="mb-3">
+            <Button
+              className="btn mr-2"
+              onClick={() => window.open(job.url, "_blank")}
+            >
+              Visit Company
+            </Button>
+            <Button
+              className="btn"
+              onClick={() => window.open(job.apply_url, "_blank")}
+            >
+              Apply Now
+            </Button>
+          </div>
+
           <DialogDescription>
-
-            <JobDescriptionPreview
-                description={job.description}
-             />
-
+            <JobDescriptionPreview description={job.description} />
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
